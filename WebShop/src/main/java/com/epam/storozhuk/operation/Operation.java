23f29c2e6ba40429c3fabca7270bb69518c8a9de
CN-EPM-ostorozhuk <oleksandr.storozhuk@alertinnovation.com>
2023-02-26 11:24:16 +0100

    Stuff clean up

--- WebShop/src/main/java/com/epam/storozhuk/operation/Operation.java
+++
@@ -1,8 +0,0 @@
-package com.epam.storozhuk.operation;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface Operation<T> {
-    T doSomeOperation(Connection connection) throws SQLException;
-}
--- WebShop/src/main/java/com/epam/storozhuk/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.epam.storozhuk.service;
-
-import com.epam.storozhuk.entity.User;
-
-public interface UserService {
-    boolean registerUser(User user);
-
-    User loginUser(String login, String password);
-}
